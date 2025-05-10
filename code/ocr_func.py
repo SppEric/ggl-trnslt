Fixed rotation preprocessing stuff

code/ocr_func.py
@@ -12,127 +12,110 @@ import numpy as np
 DEBUGGING = True
 ######## HELPER FUNCTIONS ########
 ## Unskew Image
-def unskew_image(image):
-    """Uses cv2 to detect the skew of the text in an image
-    and return the skew of the original image and a 'corrected' image
-    with the skew removed"""
+# def unskew_image(image):
+#     """Uses cv2 to detect the skew of the text in an image
+#     and return the skew of the original image and a 'corrected' image
+#     with the skew removed"""
 
-    img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # Apply skew correction using Hough transform
-    # Find the edges in the image using Canny edge detector
-    edges = cv2.Canny(img_gray, 50, 150, apertureSize=3)
+#     img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+#     # Apply skew correction using Hough transform
+#     # Find the edges in the image using Canny edge detector
+#     edges = cv2.Canny(img_gray, 50, 150, apertureSize=3)
 
 
-    # ### Non Probabilitic Hough Transform ###
-    # # Find the lines in the image using Hough transform
-    # lines = cv2.HoughLines(edges, 1, np.pi / 180, 40, None, 50, 10)
+#     # ### Non Probabilitic Hough Transform ###
+#     # # Find the lines in the image using Hough transform
+#     # lines = cv2.HoughLines(edges, 1, np.pi / 180, 40, None, 50, 10)
 
-    #     # Calculate the mean angle of the lines
-    # angles = []
-    # if lines is None:
-    #     print("No lines found")
-    #     return image, None
-    # else:
-    #     for line in lines:
-    #         rho, theta = line[0]
-    #         angles.append(theta)
-    # # # Filter angles that are very vertical and horizontal
-    # # filtered_angles = [
-    # #     theta for line in lines for rho, theta in [line[0]]
-    # #     if np.deg2rad(20) < theta < np.deg2rad(160)  # Exclude near-vertical (around 0 and 180 deg)
-    # # ]
+#     #     # Calculate the mean angle of the lines
+#     # angles = []
+#     # if lines is None:
+#     #     print("No lines found")
+#     #     return image, None
+#     # else:
+#     #     for line in lines:
+#     #         rho, theta = line[0]
+#     #         angles.append(theta)
+#     # # # Filter angles that are very vertical and horizontal
+#     # # filtered_angles = [
+#     # #     theta for line in lines for rho, theta in [line[0]]
+#     # #     if np.deg2rad(20) < theta < np.deg2rad(160)  # Exclude near-vertical (around 0 and 180 deg)
+#     # # ]
 
-    # mean_angle = np.mean(angles)
-    # # Convert the angle from radians to degrees
-    # mean_angle = mean_angle * 180 / np.pi
-    # # Create a rotation matrix using the mean angle
-    # height, width = img_gray.shape
-    # center = (width/2, height/2)
-    # rotation_matrix = cv2.getRotationMatrix2D(center, mean_angle, 1)
+#     # mean_angle = np.mean(angles)
+#     # # Convert the angle from radians to degrees
+#     # mean_angle = mean_angle * 180 / np.pi
+#     # # Create a rotation matrix using the mean angle
+#     # height, width = img_gray.shape
+#     # center = (width/2, height/2)
+#     # rotation_matrix = cv2.getRotationMatrix2D(center, mean_angle, 1)
 
-    ### Probabilistic Hough Transform ###
-    # Find the lines in the image using probabilistic Hough transform
-    # linesP is a list of lines in the form of (x_start, y_start, x_end, y_end)
-    linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, None, 50, 10)
+#     ### Probabilistic Hough Transform ###
+#     # Find the lines in the image using probabilistic Hough transform
+#     # linesP is a list of lines in the form of (x_start, y_start, x_end, y_end)
+#     linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, None, 50, 10)
 
-    # Do same computation but with linesP
-    # Calculate the mean angle of the lines
-    anglesP = []
-    if linesP is None:
-        print("No lines found")
-        return image, None
-    else:
-        for line in linesP:
-            x1, y1, x2, y2 = line[0]
-            angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
-            anglesP.append(angle)
-    mean_angleP = np.median(anglesP)
+#     # Do same computation but with linesP
+#     # Calculate the mean angle of the lines
+#     anglesP = []
+#     if linesP is None:
+#         print("No lines found")
+#         return image, None
+#     else:
+#         for line in linesP:
+#             x1, y1, x2, y2 = line[0]
+#             angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
+#             anglesP.append(angle)
+#     mean_angleP = np.median(anglesP)
 
-    ## Convert the angle from radians to degrees
-    #mean_angleP = mean_angleP * 180 / np.pi
+#     ## Convert the angle from radians to degrees
+#     #mean_angleP = mean_angleP * 180 / np.pi
     
-    # Create a rotation matrix using the mean angle
-    height, width = img_gray.shape
-    center = (width/2, height/2)
-    rotation_matrix = cv2.getRotationMatrix2D(center, mean_angleP, 1)
+#     # Create a rotation matrix using the mean angle
+#     height, width = img_gray.shape
+#     center = (width/2, height/2)
+#     rotation_matrix = cv2.getRotationMatrix2D(center, mean_angleP, 1)
 
     
-    ## Apply the rotation to the image
-    # Calculate bounding box size after rotation
-    cos = np.abs(rotation_matrix[0, 0])
-    sin = np.abs(rotation_matrix[0, 1])
-    new_w = int((height * sin) + (width * cos))
-    new_h = int((height * cos) + (width * sin))
+#     ## Apply the rotation to the image
+#     # Calculate bounding box size after rotation
+#     cos = np.abs(rotation_matrix[0, 0])
+#     sin = np.abs(rotation_matrix[0, 1])
+#     new_w = int((height * sin) + (width * cos))
+#     new_h = int((height * cos) + (width * sin))
 
-    # Adjust the rotation matrix to account for the translation
-    rotation_matrix[0, 2] += (new_w / 2) - center[0]
-    rotation_matrix[1, 2] += (new_h / 2) - center[1]
+#     # Adjust the rotation matrix to account for the translation
+#     rotation_matrix[0, 2] += (new_w / 2) - center[0]
+#     rotation_matrix[1, 2] += (new_h / 2) - center[1]
 
-    # Rotate with the new bounding box
-    img_rotated = cv2.warpAffine(image, rotation_matrix, (new_w, new_h), borderMode=cv2.BORDER_REPLICATE)
-    #image2 = cv2.rotate(img_rotated, cv2.ROTATE_90_CLOCKWISE)
-    image2 = img_rotated
+#     # Rotate with the new bounding box
+#     img_rotated = cv2.warpAffine(image, rotation_matrix, (new_w, new_h), borderMode=cv2.BORDER_REPLICATE)
+#     #image2 = cv2.rotate(img_rotated, cv2.ROTATE_90_CLOCKWISE)
+#     image2 = img_rotated
 
-    # Debugging for lines
-    #print(lines.shape)
-    print(linesP.shape)
-    for i in range(0, len(linesP)):
-        l = linesP[i][0]
+#     # Debugging for lines
+#     #print(lines.shape)
+#     print(linesP.shape)
+#     for i in range(0, len(linesP)):
+#         l = linesP[i][0]
 
-        # Convert the image to BGR format for OpenCV
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#         # Convert the image to BGR format for OpenCV
+#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Draw the lines on the image
-        cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+#         # Draw the lines on the image
+#         cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
 
-    ## Graphing the results
-    plt.figure(figsize=(12,8)) # Create a figure with a larger size
-    plt.subplot(1,2,1) # Create a subplot in the first position
-    plt.imshow(image) # Display the original image
-    # Add found hough lines to the image
-
-    plt.title("Original Image") # Add a title
-    plt.subplot(1,2,2) # Create a subplot in the second position
-    plt.imshow(image2) # Display the corrected image
-    plt.title("Skew Correction using Hough Transform") # Add a title
-    plt.show()
-    # data = pytesseract.image_to_osd(image, config=' -c min_characters_to_try=1', output_type='dict')
-    # print(data)
-    # angle = float(data.split("\n")[2].split(":")[1].strip())
-    # angle = data['rotate'] #or orientation???
-    # orientation = data['orientation']
-    # (h, w) = image.shape[:2]
-    # center = (w // 2, h // 2)
-    # # rotation matrix
-    # M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    # rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-
-    # cv2.imshow('Deskewed Image', rotated)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    #cv2.imwrite('deskewed_image.png', rotated)
-
-    return image2, rotation_matrix
+#     ## Graphing the results
+#     plt.figure(figsize=(12,8)) # Create a figure with a larger size
+#     plt.subplot(1,2,1) # Create a subplot in the first position
+#     plt.imshow(image) # Display the original image
+#     # Add found hough lines to the image
+#     plt.title("Original Image") # Add a title
+#     plt.subplot(1,2,2) # Create a subplot in the second position
+#     plt.imshow(image2) # Display the corrected image
+#     plt.title("Skew Correction using Hough Transform") # Add a title
+#     plt.show()
+#     return image2, rotation_matrix
 
 ########## EasyOCR Functions ##########
 def easy_ocr_function(image: str, from_lang: str):
@@ -149,22 +132,67 @@ def easy_ocr_function(image: str, from_lang: str):
         gpu=True
     ) 
 
-    # Read the image and get the bounding boxes and text
-    result = reader.readtext(
-        image, 
-        # General parameters
-        detail=1, 
-        batch_size=8,
-        # Detection parameters
-        paragraph=False, 
-        rotation_info=[0, 90, 180, 270], 
-        decoder="wordbeamsearch",
-        mag_ratio=1.5, # Increase the size of the image to improve accuracy
-        # Bounding box parameters 
-        width_ths=1, # Maximum horizontal distance to merge boxes - made a little wider
+    # Read the image and get the bounding boxes
+    bounding_boxes = reader.detect(
+            image,
+            mag_ratio=1.5, # Increase the size of the image to improve accuracy
+            # Bounding box parameters 
+            width_ths=1, # Maximum horizontal distance to merge boxes - made a little wider )
     )
+    # Calculate average text orientation from bounding boxes
+    angles = []
+    for points in bounding_boxes[1][0]: # bounding_boxes[1] is the list of free form bounding boxes
+        print(points)
+        top_left, top_right, _, _ = points
+        angle = np.arctan2(top_right[1] - top_left[1], top_right[0] - top_left[0]) * 180 / np.pi
+        angles.append(angle)
+    
+    mean_angle = np.median(angles) if angles else 0
+    
+    rotation_matrix = None
+    # Create rotation matrix and apply rotation if skew detected
+    if abs(mean_angle) > 0.2:  # Only correct if angle is significant
+        print("Rotation matrix necessary!")
+        height, width = image.shape[:2]
+        center = (width/2, height/2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, mean_angle, 1)
+        
+        # Calculate new image dimensions
+        cos = np.abs(rotation_matrix[0, 0])
+        sin = np.abs(rotation_matrix[0, 1])
+        new_w = int((height * sin) + (width * cos))
+        new_h = int((height * cos) + (width * sin))
+        
+        # Adjust rotation matrix
+        rotation_matrix[0, 2] += (new_w / 2) - center[0]
+        rotation_matrix[1, 2] += (new_h / 2) - center[1]
+        
+        # Apply rotation
+        image = cv2.warpAffine(image, rotation_matrix, (new_w, new_h), borderMode=cv2.BORDER_REPLICATE)
+
+        # # Display the corrected image
+        # plt.imshow(image)
+        # plt.title("Skew Correction using Average of Free Form Bounding Boxes")
+        # plt.show()
+        
+        # Re-run OCR for actual text-reading on corrected image
+        result = reader.readtext(
+            image, 
+            # General parameters
+            detail=1, 
+            batch_size=8,
+            # Detection parameters
+            paragraph=False, 
+            rotation_info=[0, 90, 180, 270], 
+            decoder="wordbeamsearch",
+            mag_ratio=1.5, # Increase the size of the image to improve accuracy
+            # Bounding box parameters 
+            width_ths=1, # Maximum horizontal distance to merge boxes - made a little wider
+        )
+    else:
+        print("No rotation matrix necessary!")
  
-    # We want to return the bounding boxes and the text, so we will extract those
+    # Extract bounding boxes and text
     bounding_pts = []
     text_list = []
     for found_text in result:
@@ -176,12 +204,10 @@ def easy_ocr_function(image: str, from_lang: str):
             print(f"Bounding box: {points}")
             print(f"Text: {text}")
             
-        # If we change to only using easyOCR, we can adjust out clustering function to use this format
-        # bounding_pts.append((top_left[0], top_left[1], bottom_right[0] - top_left[0], bottom_right[1] - top_left[1]))
         bounding_pts.append((top_left, top_right, bottom_right, bottom_left))
         text_list.append(text)
     
-    return bounding_pts, text_list
+    return bounding_pts, text_list, image, rotation_matrix
 
 def cluster_boxes(boxes: list, text_list: list):
     """Cluster the text regions based on distance, in order to get discrete
@@ -238,22 +264,20 @@ def rect_distance(p1, p2):
     return poly1.distance(poly2)
 
 ########## MAIN RETURNED FUNCTION ##########
-def image_processing(image_path: str, from_lang: str):
+def image_processing(image: np.ndarray, from_lang: str):
     """takes in an image path and returns the bounding boxes of all words in the image,
     as well as the text contained in the image"""
-    # Load the image
-    image = cv2.imread(image_path)
-    image = image[...,::-1]
 
-    # Perform image processing to remove noise, skew, etc.
-    unskewed_image, skew_num = unskew_image(image)
+
+    ## Perform image processing to remove noise, skew, etc.
+    # unskewed_image, skew_num = unskew_image(image)
     
     ## Code for using EasyOCR
     # EasyOCR has a different format for the bounding boxes and text 
     # We get the boudning boxes and text from one call to the reader
     # Bounding_pts is a list of tuples, where each tuple is (top-left, top-right, bottom-right, bottom-left)
     # Each tuple is a list of 4 points, where each point is a tuple of (x, y) coordinates
-    bounding_pts, text_list = easy_ocr_function(image, from_lang)
+    bounding_pts, text_list, image, rotation_matrix = easy_ocr_function(image, from_lang)
 
     # TODO: See if clustering is needed for EasyOCR - it may be able to do it on its own
     cluster_rectangles, clustered_text_list = cluster_boxes(bounding_pts, text_list)
@@ -281,4 +305,4 @@ def image_processing(image_path: str, from_lang: str):
         plt.show()
 
     # return a list of clusters, a list of strings, and the image
-    return cluster_rectangles, clustered_text_list, image
+    return cluster_rectangles, clustered_text_list, image, rotation_matrix

code/projection.py
@@ -4,6 +4,7 @@ import textwrap
 import matplotlib.pyplot as plt
 import cv2 as cv
 
+DEBUGGING = True
 def project_text_onto_image(image, texts, clusters):
     """
     image: Input image where the text will be projected.
@@ -108,24 +109,30 @@ def project_text_onto_image(image, texts, clusters):
             # Display displayed_text on the line canvas
             line_draw.text((0, 0), displayed_text, font=font, fill=text_color) # TODO: Add color to text, change location of text
 
-            # Convert the new line canvas to a numpy array
-            line_array = np.array(line_canvas)
+            # # Convert the new line canvas to a numpy array
+            # line_array = np.array(line_canvas)
             
             # Rotate the line array to match the angle of the rectangle
             # Calculate the angle of rotation
             angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
+
+            line_image = line_canvas.rotate(angle)
             
-            # Rotate the line array
-            line_array = cv.warpAffine(line_array, cv.getRotationMatrix2D((width // 2, height // 2), angle, 1), (width, height))
+            # # Rotate the line array
+            # line_array = cv.warpAffine(line_array, cv.getRotationMatrix2D((width // 2, height // 2), angle, 1), (width, height))
             
-            # Convert the rotated line array back to a PIL Image in integer values
-            line_image = Image.fromarray(line_array)
+            # # Convert the rotated line array back to a PIL Image in integer values
+            # line_image = Image.fromarray(line_array)
 
             # Add the line to the image
             Image.Image.paste(pil_image, line_image, (int(x1), int(y1)))
+
+            if DEBUGGING:
+                plt.imshow(pil_image)
+                plt.show()
     
         if len(remaining_text) > 0:
-            (x1,y2), (x2,y2),_,_ = lines[-1]
+            (x1,y2), (x2,y2), (x3, y3), (x4, y4) = lines[-1]
             
             displayed_text = " ".join(remaining_text)
             word_length = int(font.getlength(displayed_text)) + 1
@@ -150,21 +157,25 @@ def project_text_onto_image(image, texts, clusters):
             # Display displayed_text on the line canvas
             line_draw2.text((0, 0), displayed_text, font=font, fill=text_color) # TODO: Add color to text, change location of text
 
-            # Convert the new line canvas to a numpy array
-            line_array2 = np.array(line_canvas2)
+            # # Convert the new line canvas to a numpy array
+            # line_array2 = np.array(line_canvas2)
             
             # Rotate the line array to match the angle of the rectangle
             # Calculate the angle of rotation
             angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
             
-            # Rotate the line array
-            line_array2 = cv.warpAffine(line_array2, cv.getRotationMatrix2D((word_length // 2, height // 2), angle, 1), (word_length, height))
+            # # Rotate the line array
+            # line_array2 = cv.warpAffine(line_array2, cv.getRotationMatrix2D((word_length // 2, height // 2), angle, 1), (word_length, height))
             
             # Convert the rotated line array back to a PIL Image in integer values
-            line_image2 = Image.fromarray(line_array2)
+            line_image2 = line_canvas2.rotate(angle)
 
             # Add the line to the image
             Image.Image.paste(pil_image, line_image2, (int(x2), int(y2)))
+
+            if DEBUGGING:
+                plt.imshow(pil_image)
+                plt.show()
 
 
 