6eb4ac7c75ec3a9f6490f16f22509ae0e3fed48b
Eric J Han <30414875+SppEric@users.noreply.github.com>
2025-05-14 18:04:57 -0400

    Update ocr_func.py

--- code/ocr_func.py
+++ code/ocr_func.py
@@ -9,113 +9,7 @@ from shapely.geometry import Polygon
 import numpy as np
 
 # Variable to generate graphs of the bounding boxes for debugging
-DEBUGGING = True
-######## HELPER FUNCTIONS ########
-## Unskew Image
-# def unskew_image(image):
-#     """Uses cv2 to detect the skew of the text in an image
-#     and return the skew of the original image and a 'corrected' image
-#     with the skew removed"""
-
-#     img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-#     # Apply skew correction using Hough transform
-#     # Find the edges in the image using Canny edge detector
-#     edges = cv2.Canny(img_gray, 50, 150, apertureSize=3)
-
-
-#     # ### Non Probabilitic Hough Transform ###
-#     # # Find the lines in the image using Hough transform
-#     # lines = cv2.HoughLines(edges, 1, np.pi / 180, 40, None, 50, 10)
-
-#     #     # Calculate the mean angle of the lines
-#     # angles = []
-#     # if lines is None:
-#     #     print("No lines found")
-#     #     return image, None
-#     # else:
-#     #     for line in lines:
-#     #         rho, theta = line[0]
-#     #         angles.append(theta)
-#     # # # Filter angles that are very vertical and horizontal
-#     # # filtered_angles = [
-#     # #     theta for line in lines for rho, theta in [line[0]]
-#     # #     if np.deg2rad(20) < theta < np.deg2rad(160)  # Exclude near-vertical (around 0 and 180 deg)
-#     # # ]
-
-#     # mean_angle = np.mean(angles)
-#     # # Convert the angle from radians to degrees
-#     # mean_angle = mean_angle * 180 / np.pi
-#     # # Create a rotation matrix using the mean angle
-#     # height, width = img_gray.shape
-#     # center = (width/2, height/2)
-#     # rotation_matrix = cv2.getRotationMatrix2D(center, mean_angle, 1)
-
-#     ### Probabilistic Hough Transform ###
-#     # Find the lines in the image using probabilistic Hough transform
-#     # linesP is a list of lines in the form of (x_start, y_start, x_end, y_end)
-#     linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, None, 50, 10)
-
-#     # Do same computation but with linesP
-#     # Calculate the mean angle of the lines
-#     anglesP = []
-#     if linesP is None:
-#         print("No lines found")
-#         return image, None
-#     else:
-#         for line in linesP:
-#             x1, y1, x2, y2 = line[0]
-#             angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
-#             anglesP.append(angle)
-#     mean_angleP = np.median(anglesP)
-
-#     ## Convert the angle from radians to degrees
-#     #mean_angleP = mean_angleP * 180 / np.pi
-    
-#     # Create a rotation matrix using the mean angle
-#     height, width = img_gray.shape
-#     center = (width/2, height/2)
-#     rotation_matrix = cv2.getRotationMatrix2D(center, mean_angleP, 1)
-
-    
-#     ## Apply the rotation to the image
-#     # Calculate bounding box size after rotation
-#     cos = np.abs(rotation_matrix[0, 0])
-#     sin = np.abs(rotation_matrix[0, 1])
-#     new_w = int((height * sin) + (width * cos))
-#     new_h = int((height * cos) + (width * sin))
-
-#     # Adjust the rotation matrix to account for the translation
-#     rotation_matrix[0, 2] += (new_w / 2) - center[0]
-#     rotation_matrix[1, 2] += (new_h / 2) - center[1]
-
-#     # Rotate with the new bounding box
-#     img_rotated = cv2.warpAffine(image, rotation_matrix, (new_w, new_h), borderMode=cv2.BORDER_REPLICATE)
-#     #image2 = cv2.rotate(img_rotated, cv2.ROTATE_90_CLOCKWISE)
-#     image2 = img_rotated
-
-#     # Debugging for lines
-#     #print(lines.shape)
-#     print(linesP.shape)
-#     for i in range(0, len(linesP)):
-#         l = linesP[i][0]
-
-#         # Convert the image to BGR format for OpenCV
-#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-#         # Draw the lines on the image
-#         cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
-
-#     ## Graphing the results
-#     plt.figure(figsize=(12,8)) # Create a figure with a larger size
-#     plt.subplot(1,2,1) # Create a subplot in the first position
-#     plt.imshow(image) # Display the original image
-#     # Add found hough lines to the image
-#     plt.title("Original Image") # Add a title
-#     plt.subplot(1,2,2) # Create a subplot in the second position
-#     plt.imshow(image2) # Display the corrected image
-#     plt.title("Skew Correction using Hough Transform") # Add a title
-#     plt.show()
-#     return image2, rotation_matrix
+DEBUGGING = False
 
 ########## EasyOCR Functions ##########
 def easy_ocr_function(image: str, from_lang: str):
@@ -126,6 +20,9 @@ def easy_ocr_function(image: str, from_lang: str):
     The confidence score is the last element of each tuple
     """
     # TODO: Create dictioniary to map standardized language codes to EasyOCR language codes
+    if from_lang == "zh":
+        from_lang = "ch_sim"
+    
     reader = easyocr.Reader(
         [from_lang], 
         # GPU stuff
@@ -135,9 +32,9 @@ def easy_ocr_function(image: str, from_lang: str):
     # Read the image and get the bounding boxes
     bounding_boxes = reader.detect(
             image,
-            mag_ratio=1, # Increase the size of the image to improve accuracy
+            mag_ratio=1.75, # Increase the size of the image to improve accuracy
             # Bounding box parameters 
-            width_ths=1.5, # Maximum horizontal distance to merge boxes - made a little wider)
+            width_ths=2, # Maximum horizontal distance to merge boxes - made a little wider)
     )
 
 
@@ -164,8 +61,6 @@ def easy_ocr_function(image: str, from_lang: str):
     # Count whether there is a large amount of straight text or text that is rotated
     straight_text_count = len(bounding_boxes[0][0])
     rotated_text_count = len(bounding_boxes[1][0])
-    print(straight_text_count)
-    print(rotated_text_count)
 
     rotation_matrix = None
     if straight_text_count < rotated_text_count: # This is naive, potentially will not work
@@ -216,13 +111,11 @@ def easy_ocr_function(image: str, from_lang: str):
         batch_size=8,
         # Detection parameters
         paragraph=False, 
-        rotation_info=[0, 90, 180, 270], 
+        rotation_info=[0], 
         decoder="wordbeamsearch",
-        mag_ratio=1, # Increase the size of the image to improve accuracy
+        mag_ratio=1.75, # Increase the size of the image to improve accuracy
         # Bounding box parameters 
         width_ths=2, # Maximum horizontal distance to merge boxes - made a little wider,
-        add_margin=0.05,
-        link_threshold=0.2
     )
  
     # Extract bounding boxes and text
@@ -285,18 +178,7 @@ def cluster_boxes(boxes: list, text_list: list):
     
         clusters[label].append(rect)
         clusters_words[label].append(words)
-
-    # def merge_rects(rect_list):
-    #     xs = [x for x, y, w, h in rect_list]
-    #     ys = [y for x, y, w, h in rect_list]
-    #     xws = [x + w for x, y, w, h in rect_list]
-    #     yhs = [y + h for x, y, w, h in rect_list]
-    #     x_min, y_min = min(xs), min(ys)
-    #     x_max, y_max = max(xws), max(yhs)
-    #     # together = " ".join(words_list)
-    #     return (int(x_min), int(y_min), int(x_max - x_min), int(y_max - y_min))
     
-    # merged_rects = [(merge_rects(rlist), len(rlist)) for rlist in clusters.values()]
     merged_text = [" ".join(word_list) for word_list in clusters_words.values()]
     #list of lists of rectangle coordinates, so its a big list and each list within it is one cluster
     #in other words a list of what rectangles go in each cluster.
@@ -323,10 +205,6 @@ def rect_distance(p1, p2):
 def image_processing(image: np.ndarray, from_lang: str):
     """takes in an image path and returns the bounding boxes of all words in the image,
     as well as the text contained in the image"""
-
-
-    ## Perform image processing to remove noise, skew, etc.
-    # unskewed_image, skew_num = unskew_image(image)
     
     ## Code for using EasyOCR
     # EasyOCR has a different format for the bounding boxes and text 
@@ -338,26 +216,21 @@ def image_processing(image: np.ndarray, from_lang: str):
     # TODO: See if clustering is needed for EasyOCR - it may be able to do it on its own
     cluster_rectangles, clustered_text_list = cluster_boxes(bounding_pts, text_list)
 
-    # if DEBUGGING:
-    #     fig, ax = plt.subplots()
-    #     ax.imshow(image)
+    if DEBUGGING:
+        fig, ax = plt.subplots()
+        ax.imshow(image)
 
-    #     # Original rectangles in blue
-    #     for top_left, top_right, bottom_right, bottom_left in bounding_pts:
-    #         # Draw the points in blue on top of the original image
-    #         ax.plot([top_left[0], top_right[0], bottom_right[0], bottom_left[0], top_left[0]], 
-    #                 [top_left[1], top_right[1], bottom_right[1], bottom_left[1], top_left[1]], 'bo-')
+        # Original rectangles in blue
+        for top_left, top_right, bottom_right, bottom_left in bounding_pts:
+            # Draw the points in blue on top of the original image
+            ax.plot([top_left[0], top_right[0], bottom_right[0], bottom_left[0], top_left[0]], 
+                    [top_left[1], top_right[1], bottom_right[1], bottom_left[1], top_left[1]], 'bo-')
             
+        print(clustered_text_list)
 
-    #         # w = bottom_right[0] - top_left[0]   
-    #         # h = bottom_right[1] - top_left[1]
-    #         # ax.add_patch(patches.Rectangle((top_left[0], top_left[1]), w, h, edgecolor='blue', facecolor='none', linewidth=1, linestyle='--'))
-
-    #     print(clustered_text_list)
-
-    #     plt.axis('equal')
-    #     plt.title("Clustered Text")
-    #     plt.show()
+        plt.axis('equal')
+        plt.title("Clustered Text")
+        plt.show()
 
     # return a list of clusters, a list of strings, and the image
     return cluster_rectangles, clustered_text_list, image, rotation_matrix
